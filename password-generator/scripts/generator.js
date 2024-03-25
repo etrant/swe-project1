153b4fcc1ef763764ca1dac1d5131784f6767ae7
Ethan <30396562+etrant@users.noreply.github.com>
2024-03-24 23:09:12 -0400

    Base code for password generator
    Includes project scaffolding

---
+++ password-generator/scripts/generator.js
@@ -0,0 +1,58 @@
+
+let config = createConfiguration();
+
+function createConfiguration(selectedOptions = ["upper","numbers"], passwordSize = 16){
+    let lowercase_charset = "abcdefghijklmnopqrstuvwxyz";
+    let uppercase_charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numeric_charset = "0123456789";
+    let symbols_charset = "@!$#&*";
+    
+    let options = {
+        "lower": lowercase_charset,
+        "upper": uppercase_charset,
+        "numbers": numeric_charset,
+        "symbols": symbols_charset
+    };
+
+    let charset = "";
+
+    for(let i = 0; i < selectedOptions.length; i++){
+        if (selectedOptions[i] in options) {
+            let selection = options[selectedOptions[i]];
+            charset = charset.concat(selection);
+        }
+    }
+
+    let charsetSize = charset.length;
+
+    return {charset,charsetSize,selectedOptions,passwordSize};
+}
+
+
+function createPassword(configuration) {
+    let password = "";
+    for (let i = 0; i < configuration.passwordSize; i++) {
+        
+        password += configuration.charset.charAt(Math.floor(Math.random() * configuration.charsetSize));
+    }
+    addToPasswordHistory(password);
+    return password;
+}
+
+
+function addToPasswordHistory(password){
+    let password_list = [password];
+    if (sessionStorage.getItem("password_list") === null) {
+        sessionStorage.setItem("password_list", JSON.stringify(password_list));
+    }
+    else {
+        let old_list = getPasswordHistory();
+        let new_list = JSON.stringify(password_list.concat(old_list));
+        sessionStorage.setItem("password_list", new_list);
+    }
+}
+
+
+function getPasswordHistory(){
+    return JSON.parse(sessionStorage.getItem("password_list"));
+}
---
+++ password-generator/scripts/helpers.js
@@ -0,0 +1,32 @@
+ function clearHistory() {
+        sessionStorage.clear();
+        const history = document.getElementById("history");
+        history.innerHTML = "";
+      }
+
+function generate() {
+    let passwordSize = document.getElementById("passwordSize").value;
+    // Gather selected options from checkbox
+    let selected = []
+    let checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+    for (let i = 0; i < checkboxes.length; i++) {
+        selected.push(checkboxes[i].value)
+    }
+
+    // Generate the configuration using the chosen values
+    let config = createConfiguration(selected, passwordSize);
+
+    // Find the div with the id "main"
+    const box = document.getElementById("main");
+    // Overwrite it's text with the value of the generated password
+    box.innerHTML = createPassword(config);
+
+    // Find the div with the id "history" and clear it (prevents duplicates)
+    const history = document.getElementById("history");
+    history.innerHTML = "";
+
+    // Add passwords stored in sessionStorage to the webpage.
+    getPasswordHistory().forEach((password) => {
+        history.innerHTML += `<p>${password}</p>`;
+    });
+}
