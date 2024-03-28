f9edbef928a359d5cd647816d6e748ca37847943
Zkarobi <147928620+Zkarobi@users.noreply.github.com>
2024-03-27 20:04:25 -0400

    reset history buttons

--- password-generator/scripts/ui.js
+++ password-generator/scripts/ui.js
@@ -1,17 +1,18 @@
 // Reset to Default Settings functionality
 function resetToDefaultSettings() {
-     // Reset password size to 16
+
+     //reset password size to 16
      document.getElementById("passwordSize").value = 16;
-     document.querySelector('output[name="sizeValue"]').value = 16; // Update the displayed value next to the slider
+     document.querySelector('output[name="sizeValue"]').value = 16;
  
-     // Reset checkboxes: Check 'upper' and 'numbers', uncheck 'lower' and 'symbols'
+     //reset checkboxes
      document.querySelector('input[value="upper"]').checked = true;
      document.querySelector('input[value="numbers"]').checked = true;
      document.querySelector('input[value="lower"]').checked = false;
      document.querySelector('input[value="symbols"]').checked = false;
 }
 
-// Event listener for the Reset button
+//event listener for the reset button
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("resetToDefaultsButton").addEventListener("click", resetToDefaultSettings);
 });
